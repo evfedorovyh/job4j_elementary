2e07a2e0a6d967902cff55c3ac429d07b2b208ef
Home91 <jhon2002@mail.ru>
2024-12-23 00:14:40 +0300

    Разбор первой программы по "взрослому" [#505151]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,9 +2,9 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        int i=2;
-        while (i<10){
-            System.out.println("1*" + i + "=" + 1*i);
+        int i = 2;
+        while (i < 10) {
+            System.out.println("1*" + i + "=" + 1 * i);
             i++;
         }
     }
