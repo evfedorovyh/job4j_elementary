2b20c1e2cbc2e8e6e286d026d79a727b4312869b
Home91 <jhon2002@mail.ru>
2024-12-23 00:03:13 +0300

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Fedorovyh Evgeny");
+        System.out.println("28.01.1982");
+    }
+}
