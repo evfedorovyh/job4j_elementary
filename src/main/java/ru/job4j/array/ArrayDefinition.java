acca02d82d4bc1849b7aa509c54e16b0c82c0296
Home91 <jhon2002@mail.ru>
2024-12-24 10:55:01 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages[] равен: " + ages.length);
         System.out.println("Размер массива surnames[] равен: " + surnames.length);
         System.out.println("Размер массива prices[] равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Alexandr Pushkin";
+        names[1] = "Mihail Bulgakov";
+        names[2] = "Fedor Dostoevsky";
+        names[3] = "Sergey Esenin";
+        for (String s: names) {
+            System.out.println(s);
+        }
     }
 }
